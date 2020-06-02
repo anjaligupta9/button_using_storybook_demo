a97736f7804b190e27c1aacce4cc723db0e5a586
Anjali <anjali.gupta091205@gmail.com>
2020-06-02 14:56:26 +0530

    adding button component

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Button.css';
+
+const Button = props => (
+  <button onClick={props.onClick} style={props.style}>
+    {props.label && <span>{props.label}</span>}
+  </button>
+)
+export default Button;
